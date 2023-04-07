7db1165f0e71a18963d97380be4cae2c82061565
César Bitarães <caabitaraes@gmail.com>
2023-04-06 22:59:57 -0300

    First commit

---
+++ Models/Gasto.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleFinanceiroApi.Models;
+
+public class Gasto
+{
+    public int Id { get; set; }
+    public string Descricao { get; set; }
+    public Decimal Valor { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+using ControleFinanceiroApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApiDataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
